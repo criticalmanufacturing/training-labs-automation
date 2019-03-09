c7397818c6fd72a398961b456b560092fc082a30
Pedro Salgueiro <pedro.salgueiro@outlook.com>
2019-03-09 22:06:26 +0000

    fix invalid charaters on the script

--- SingleNode/0.1.EnableDhcp.ps1
+++ SingleNode/0.1.EnableDhcp.ps1
@@ -4,14 +4,10 @@ $labPrefix = "SN"
 $domain = $settings.domain
 $user = $settings.username
 $password = $settings.password
+$password = $password | ConvertTo-SecureString  -AsPlainText -Force
 $adminCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "$($settings.shortDomain)\$($user)", $password 
 
-$password = $password | ConvertTo-SecureString  -AsPlainText -Force
-
-Invoke-LabCommand -ComputerName SNAPPSRV01 `
-    -ActivityName 'Prepare SQL Server Distribution Media' `
-    -ArgumentList "$($labPrefix)DC1", "$($labPrefix)DC1.($domain)", $domain, $adminCredential `
-    -ScriptBlock {
+Invoke-LabCommand -ComputerName SNAPPSRV01 -ActivityName 'Prepare SQL Server Distribution Media' -ArgumentList "$($labPrefix)DC1", "$($labPrefix)DC1.($domain)", $domain, $adminCredential -ScriptBlock { 
 param
 (
     [string]$dhcpDnsName,
@@ -25,12 +21,12 @@ netsh dhcp add securitygroups
 Restart-service dhcpserver
 
 Add-DhcpServerInDC -DnsName $dhcpDnsName -IPAddress 192.168.80.3
-Set-ItemProperty –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 –Name ConfigurationState –Value 2
-Set-DhcpServerv4DnsSetting -ComputerName $dhcpDnsName -DynamicUpdates "Always" -DeleteDnsRRonLeaseExpiry $True
+Set-ItemProperty -Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 -Name ConfigurationState -Value 2
+Set-DhcpServerv4DnsSetting -ComputerName $dhcpDnsName -DynamicUpdates 'Always' -DeleteDnsRRonLeaseExpiry $True
 Set-DhcpServerDnsCredential -Credential $Credential -ComputerName $dhcpDnsName
-Add-DhcpServerv4Scope -name "Intranet" -StartRange 192.168.80.30 -EndRange 192.168.80.60 -SubnetMask 255.255.0.0 -State Active
+Add-DhcpServerv4Scope -name 'Intranet' -StartRange 192.168.80.30 -EndRange 192.168.80.60 -SubnetMask 255.255.0.0 -State Active
 Add-DhcpServerv4ExclusionRange -ScopeID 192.168.80.0 -StartRange 192.168.80.1 -EndRange 192.168.80.15
-Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.80.3 -ScopeID 192.168.80.0 -ComputerName 
+Set-DhcpServerv4OptionValue -OptionID 3 -Value 192.168.80.3 -ScopeID 192.168.80.0 -ComputerName $dhcpDnsName
 Set-DhcpServerv4OptionValue -DnsDomain $dnsDomain -DnsServer 192.168.80.3
 
 }
